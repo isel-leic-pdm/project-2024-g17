24a41a12f1d9d73604d0b29071a43cb0dfd4ff59
Joao Cardoso <a50465@alunos.isel.pt>
2024-10-06 20:02:12 +0100

    feat: ChiMP logo

--- app/src/main/java/com/leic52dg17/chimp/ui/views/AboutView.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/views/AboutView.kt
@@ -1,2 +1,28 @@
 package com.leic52dg17.chimp.ui.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AboutView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AboutViewPreview() {
+    AboutView()
+}
