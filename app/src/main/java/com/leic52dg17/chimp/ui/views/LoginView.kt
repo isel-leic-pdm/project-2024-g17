aae455b188a2c69bea3db8d9f6a3fcec06c234a0
HarvyDev <franciscolbantunes@gmail.com>
2024-10-07 20:01:13 +0100

    Landing page progress

--- app/src/main/java/com/leic52dg17/chimp/ui/views/LoginView.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/views/LoginView.kt
@@ -1,31 +1,25 @@
 package com.leic52dg17.chimp.ui.views
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.leic52dg17.chimp.R
 import com.leic52dg17.chimp.ui.components.AuthenticationButton
 
@@ -36,8 +30,41 @@ fun LandingView(
     onSignUpClick: () -> Unit
 ) {
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxSize(),
     ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painter = ColorPainter(Color.Blue),
+                contentDescription = null,
+                modifier = modifier
+                    .size(200.dp)
+            )
+        }
+
+        Column(
+            modifier = modifier.padding(bottom = 20.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.landing_welcome_message_en),
+                fontWeight = FontWeight.Bold,
+                fontSize = 35.sp,
+                modifier = modifier.fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.landing_description_en),
+                fontSize = 15.sp,
+                modifier = modifier.fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+        }
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = modifier.fillMaxWidth()
@@ -47,7 +74,7 @@ fun LandingView(
                 backgroundColor = MaterialTheme.colorScheme.onPrimary,
                 textColor = MaterialTheme.colorScheme.secondary,
                 text = stringResource(R.string.log_in_button_text_en),
-                modifier = modifier,
+                border = BorderStroke(1.dp, Color.Black), // REMOVE LATER
             )
 
             AuthenticationButton(
