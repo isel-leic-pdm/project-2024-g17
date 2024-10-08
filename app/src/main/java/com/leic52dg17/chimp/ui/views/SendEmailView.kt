bf88c297db17d975900ae0c1a55455bed5615940
Joao Cardoso <a50465@alunos.isel.pt>
2024-10-08 11:37:56 +0100

    fi: removed unused imports feat: missing strings.xml

--- app/src/main/java/com/leic52dg17/chimp/ui/views/SendEmailView.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/views/SendEmailView.kt
@@ -22,11 +22,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.leic52dg17.chimp.R
 import com.leic52dg17.chimp.ui.theme.ChIMPTheme
 
