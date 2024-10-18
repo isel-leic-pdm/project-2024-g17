a967966d2182007b8f9988fbabbd5e91622f1977
HarvyDev <franciscolbantunes@gmail.com>
2024-10-18 09:50:56 +0100

    channel info view progress

--- app/src/main/java/com/leic52dg17/chimp/ui/views/channel/ChannelInfoView.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/views/channel/ChannelInfoView.kt
@@ -1,13 +1,18 @@
 package com.leic52dg17.chimp.ui.views.channel
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
-import com.leic52dg17.chimp.R
+import coil.compose.rememberAsyncImagePainter
+import com.leic52dg17.chimp.model.User
 import com.leic52dg17.chimp.model.channel.Channel
+import com.leic52dg17.chimp.model.message.Message
+import java.math.BigInteger
+import java.time.Instant
 
 @Composable
 fun ChannelInfoView(
@@ -17,10 +22,10 @@ fun ChannelInfoView(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Column {
-//            Image(
-//                painter = painterResource(id = R.drawable.channel_image),
-//                contentDescription = "Channel Image"
-//            )
+            Image(
+                painter = rememberAsyncImagePainter(channel.channelIconUrl),
+                contentDescription = "Channel Image"
+            )
             Text(
                 text = channel.displayName,
                 fontFamily = MaterialTheme.typography.displayLarge.fontFamily,
@@ -35,10 +40,20 @@ fun ChannelInfoView(
 @Composable
 fun ChannelInfoViewPreview() {
     ChannelInfoView(
-        channel = Channel(
+        Channel(
+            channelId = 1,
             displayName = "Channel Name",
-            channelIcon = R.mipmap.chimp_white_logo_foreground,
-
+            channelIconUrl = "https://picsum.photos/300/300",
+            users = listOf(
+                User(1, "User1", "displayname1"),
+                User(2, "User2", "displayname2")
+            ),
+            messages = listOf(
+                Message(1,1, "Hello", BigInteger.valueOf(Instant.now().toEpochMilli())),
+                Message(2,1, "Hi", BigInteger.valueOf(Instant.now().toEpochMilli())),
+                Message(1, 1, "How are you", BigInteger.valueOf(Instant.now().toEpochMilli()))
+            ),
+            channelIconContentDescription = null
         )
     )
 }
