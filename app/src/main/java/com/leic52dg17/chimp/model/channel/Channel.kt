6c19e12b4ee0419b6c47b192a5936af16fc63f87
HarvyDev <franciscolbantunes@gmail.com>
2024-10-18 09:45:10 +0100

    fixed channel model and adapted subscribed channels view

--- app/src/main/java/com/leic52dg17/chimp/model/channel/Channel.kt
+++ app/src/main/java/com/leic52dg17/chimp/model/channel/Channel.kt
@@ -1,12 +1,13 @@
 package com.leic52dg17.chimp.model.channel
 
 import com.leic52dg17.chimp.model.User
-import com.leic52dg17.chimp.model.common.ImageResource
 import com.leic52dg17.chimp.model.message.Message
 
 data class Channel(
+    val channelId: Int,
     val displayName: String,
     val messages: List<Message>,
     val users: List<User>,
-    val channelIcon: ImageResource
+    val channelIconUrl: String,
+    val channelIconContentDescription: String?,
 )
