02c079bccc60293178ab4e2a4f65b6591eef599a
João Cardoso <122165256+CardosoDev04@users.noreply.github.com>
2024-10-25 08:40:32 +0100

    fix/conflicts (#37)

--- app/src/main/java/com/leic52dg17/chimp/ui/views/MessageView.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/views/MessageView.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
@@ -36,7 +35,6 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
