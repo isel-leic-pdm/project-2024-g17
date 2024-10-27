ef63de21c2a734b4e4aa03a22768e9d61933777a
Francisco Antunes <114776036+HarvyDev@users.noreply.github.com>
2024-10-27 20:29:15 +0000

    Feat: Authentication viewmodel (#45)

--- app/src/main/java/com/leic52dg17/chimp/ui/screens/authentication/AuthenticationScreenState.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/screens/authentication/AuthenticationScreenState.kt
@@ -2,7 +2,7 @@ package com.leic52dg17.chimp.ui.screens.authentication
 
 sealed interface AuthenticationScreenState {
     data object Landing : AuthenticationScreenState
-    data object SignUp : AuthenticationScreenState
+    data class SignUp(val isDialogOpen: Boolean, val errorMessage: String? = null) : AuthenticationScreenState
     data class Login(val isDialogOpen: Boolean, val errorMessage: String? = null) : AuthenticationScreenState
     data object LoggingIn: AuthenticationScreenState
     data object ForgotPassword : AuthenticationScreenState
