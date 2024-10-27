8d57764de6c22a4a85e39a8bb91c33ff25f9a5f1
HarvyDev <franciscolbantunes@gmail.com>
2024-10-27 20:23:05 +0000

    Authentication viewmodel implementation with fake service

--- app/src/main/java/com/leic52dg17/chimp/http/services/auth/implementations/FakeAuthenticationService.kt
+++ app/src/main/java/com/leic52dg17/chimp/http/services/auth/implementations/FakeAuthenticationService.kt
@@ -2,12 +2,29 @@ package com.leic52dg17.chimp.http.services.auth.implementations
 
 import com.leic52dg17.chimp.http.services.auth.IAuthenticationService
 import com.leic52dg17.chimp.http.services.auth.results.UserLoginResult
+import com.leic52dg17.chimp.http.services.auth.results.UserSignUpResult
 import com.leic52dg17.chimp.model.auth.AuthenticatedUser
 import com.leic52dg17.chimp.model.common.success
 import com.leic52dg17.chimp.model.user.User
+import kotlinx.coroutines.delay
 
 class FakeAuthenticationService : IAuthenticationService {
     override suspend fun loginUser(username: String, password: String): UserLoginResult {
+        delay(2000)
+        return success(
+            AuthenticatedUser(
+                "example_token",
+                User(
+                    1,
+                    "username1",
+                    "User 1"
+                )
+            )
+        )
+    }
+
+    override suspend fun signUpUser(username: String, password: String): UserSignUpResult {
+        delay(2000)
         return success(
             AuthenticatedUser(
                 "example_token",
--- app/src/main/java/com/leic52dg17/chimp/ui/screens/authentication/AuthenticationScreen.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/screens/authentication/AuthenticationScreen.kt
@@ -1,9 +1,14 @@
 package com.leic52dg17.chimp.ui.screens.authentication
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.leic52dg17.chimp.http.services.auth.implementations.FakeAuthenticationService
 import com.leic52dg17.chimp.http.services.channel.implementations.FakeChannelService
+import com.leic52dg17.chimp.ui.components.overlays.LoadingOverlay
 import com.leic52dg17.chimp.ui.theme.ChIMPTheme
 import com.leic52dg17.chimp.ui.viewmodels.screen.AuthenticationScreenViewModel
 import com.leic52dg17.chimp.ui.viewmodels.screen.MainScreenViewModel
@@ -16,24 +21,48 @@ fun AuthenticationScreen(viewModel: AuthenticationScreenViewModel) {
     ChIMPTheme {
         val currentState = viewModel.state
 
+        var isLoading by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        if (isLoading) {
+            LoadingOverlay()
+        }
+
         when (currentState) {
-            is AuthenticationScreenState.Landing -> LandingView(
-                onLogInClick = { viewModel.state = AuthenticationScreenState.Login(false) },
-                onSignUpClick = { viewModel.state = AuthenticationScreenState.SignUp },
-            )
-            is AuthenticationScreenState.Login -> LoginView(
-                onLogInClick = { /* TODO() */ },
-                onSignUpClick = { viewModel.state = AuthenticationScreenState.SignUp },
-                onBackClick = { viewModel.state = AuthenticationScreenState.Landing },
-            )
-            is AuthenticationScreenState.SignUp -> SignUpView(
-                onSignUpClick = { /* TODO() */ },
-                onLogInClick = { viewModel.state = AuthenticationScreenState.Login(false) },
-                onBackClick = { viewModel.state = AuthenticationScreenState.Landing },
-            )
+            is AuthenticationScreenState.Landing -> {
+                isLoading = false
+                LandingView(
+                    onLogInClick = { viewModel.transition(AuthenticationScreenState.Login(false)) },
+                    onSignUpClick = { viewModel.transition(AuthenticationScreenState.SignUp(false)) },
+                )
+            }
+
+            is AuthenticationScreenState.Login -> {
+                isLoading = false
+                LoginView(
+                    onLogInClick = { username, password -> viewModel.loginUser(username, password) },
+                    onSignUpClick = { viewModel.transition(AuthenticationScreenState.SignUp(false)) },
+                    onBackClick = { viewModel.transition(AuthenticationScreenState.Landing) },
+                )
+            }
+
+            is AuthenticationScreenState.SignUp -> {
+                isLoading = false
+                SignUpView(
+                    onSignUpClick = { username, password -> viewModel.signUpUser(username, password) },
+                    onLogInClick = { viewModel.transition(AuthenticationScreenState.Login(false)) },
+                    onBackClick = { viewModel.transition(AuthenticationScreenState.Landing) },
+                )
+            }
+
             is AuthenticationScreenState.ForgotPassword -> throw NotImplementedError()
-            AuthenticationScreenState.Authenticated -> TODO()
-            AuthenticationScreenState.LoggingIn -> TODO()
+
+            is AuthenticationScreenState.Authenticated -> TODO()
+
+            is AuthenticationScreenState.LoggingIn -> {
+                isLoading = true
+            }
         }
     }
 }
