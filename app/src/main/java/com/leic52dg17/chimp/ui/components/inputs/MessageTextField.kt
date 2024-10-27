28b85c1ff59a91f23ec149e9899b34fd27ae92fc
João Cardoso <122165256+CardosoDev04@users.noreply.github.com>
2024-10-27 20:24:26 +0000

    features/connect-message-view (#44)

--- app/src/main/java/com/leic52dg17/chimp/ui/components/inputs/MessageTextField.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/components/inputs/MessageTextField.kt
@@ -1,35 +1,45 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.leic52dg17.chimp.R
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessageTextField(
     messageText: String,
     onMessageTextChange: (String) -> Unit,
-    modifier: Modifier = Modifier
+    textColor: Color = MaterialTheme.colorScheme.onSecondaryContainer,
+    inputBoxColor: Color = MaterialTheme.colorScheme.secondaryContainer,
+    modifier: Modifier = Modifier,
+    minLines: Int = 1,
+    maxLines: Int = 5
 ) {
-    OutlinedTextField(
-        value = messageText,
-        onValueChange = onMessageTextChange,
-        label = { Text(stringResource(R.string.message_text_field_en)) },
-        modifier = modifier
-            .padding(1.dp),
-        shape = RoundedCornerShape(10.dp),
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = MaterialTheme.colorScheme.primary,
-            unfocusedBorderColor = MaterialTheme.colorScheme.secondary,
-
+    Box(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        TextField(
+            value = messageText,
+            onValueChange = onMessageTextChange,
+            modifier = modifier.fillMaxWidth(),
+            maxLines = maxLines,
+            minLines = minLines,
+            colors = TextFieldDefaults.textFieldColors(
+                focusedTextColor = textColor,
+                containerColor = inputBoxColor,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
         )
-    )
+    }
 }
