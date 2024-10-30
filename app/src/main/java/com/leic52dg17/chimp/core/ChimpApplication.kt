380fd3c7a12a0c3beaddab21366561cb580ca775
Francisco Antunes <114776036+HarvyDev@users.noreply.github.com>
2024-10-30 23:11:15 +0000

    feat: Profile view (#65)

--- app/src/main/java/com/leic52dg17/chimp/core/ChimpApplication.kt
+++ app/src/main/java/com/leic52dg17/chimp/core/ChimpApplication.kt
@@ -7,6 +7,8 @@ import com.leic52dg17.chimp.http.services.channel.IChannelService
 import com.leic52dg17.chimp.http.services.channel.implementations.FakeChannelService
 import com.leic52dg17.chimp.http.services.message.IMessageService
 import com.leic52dg17.chimp.http.services.message.implementations.FakeMessageService
+import com.leic52dg17.chimp.http.services.user.IUserService
+import com.leic52dg17.chimp.http.services.user.implementations.FakeUserService
 
 const val TAG = "CHIMP"
 
@@ -14,6 +16,7 @@ interface DependenciesContainer {
     val channelService: IChannelService
     val messageService: IMessageService
     val authenticationService: IAuthenticationService
+    val userService: IUserService
 }
 
 class ChimpApplication : Application(), DependenciesContainer {
@@ -25,5 +28,8 @@ class ChimpApplication : Application(), DependenciesContainer {
     }
     override val authenticationService: IAuthenticationService by lazy {
         FakeAuthenticationService()
+    }
+    override val userService: IUserService by lazy {
+        FakeUserService()
     }
 }
--- app/src/main/java/com/leic52dg17/chimp/ui/screens/main/MainViewSelector.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/screens/main/MainViewSelector.kt
@@ -20,6 +20,7 @@ import com.leic52dg17.chimp.ui.components.nav.BottomNavbar
 import com.leic52dg17.chimp.ui.components.overlays.LoadingOverlay
 import com.leic52dg17.chimp.ui.theme.ChIMPTheme
 import com.leic52dg17.chimp.ui.viewmodels.screen.MainViewSelectorViewModel
+import com.leic52dg17.chimp.ui.views.UserInfoView
 import com.leic52dg17.chimp.ui.views.channel.ChannelInfoView
 import com.leic52dg17.chimp.ui.views.channel.ChannelMessageView
 import com.leic52dg17.chimp.ui.views.create_channel.CreateChannelView
@@ -65,12 +66,22 @@ fun MainViewSelector(
             bottomBar = {
                 if (isNavBarShown) {
                     BottomNavbar(
-                        selectedNavIcon,
-                        Modifier
+                        selectedIcon = selectedNavIcon,
+                        onClickProfile = {
+                            selectedNavIcon = "profile"
+                            val currentUser = authenticatedUser?.user
+                            if (currentUser != null) {
+                                viewModel.getUserProfile(currentUser.userId)
+                            }
+                        },
+                        onClickMessages = {
+                            selectedNavIcon = "chats"
+                        },
+                        onClickSettings = {
+                            selectedNavIcon = "settings"
+                        },
+                        rowModifier = Modifier
                             .padding(bottom = 32.dp),
-                        { selectedNavIcon = "profile" },
-                        { selectedNavIcon = "chats" },
-                        { selectedNavIcon = "settings" }
                     )
                 }
             }
@@ -191,7 +202,19 @@ fun MainViewSelector(
                             onBackClick = { /*TODO()*/ },
                             onAddToUserChannelClick = { /*TODO()*/ },
                             onRemoveUser = { /*TODO()*/ },
-                            onUserClick = { /*TODO()*/ },
+                            onUserClick = { userId -> viewModel.getUserProfile(userId) },
+                        )
+                    }
+
+                    is MainViewSelectorState.UserInfo -> {
+                        val currentState = (viewModel.state as MainViewSelectorState.UserInfo)
+                        UserInfoView(
+                            user = currentState.user,
+                            authenticatedUser = authenticatedUser,
+                            onBackClick = {
+                                viewModel.transition(MainViewSelectorState.SubscribedChannels(false))
+                            },
+                            onLogoutClick = { viewModel.logout() },
                         )
                     }
                 }
