73211bebf56911cb0b49a534adc07c319f003d0b
João Cardoso <122165256+CardosoDev04@users.noreply.github.com>
2024-11-18 12:29:30 +0000

    tests/main-view-model-basic (#86)

---
+++ app/src/androidTest/java/com/leic52dg17/chimp/ui/viewmodel/MainViewSelectorViewModelTests.kt
@@ -0,0 +1,53 @@
+package com.leic52dg17.chimp.ui.viewmodel
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.leic52dg17.chimp.http.services.channel.implementations.FakeChannelService
+import com.leic52dg17.chimp.http.services.message.implementations.FakeMessageService
+import com.leic52dg17.chimp.http.services.user.implementations.FakeUserService
+import com.leic52dg17.chimp.model.auth.AuthenticatedUser
+import com.leic52dg17.chimp.ui.screens.main.MainViewSelectorState
+import com.leic52dg17.chimp.ui.viewmodels.screen.MainViewSelectorViewModel
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class MainViewSelectorViewModelTests {
+    private lateinit var vm: MainViewSelectorViewModel
+    private val authenticatedUser = AuthenticatedUser()
+    private val channelService = FakeChannelService()
+    private val messageService = FakeMessageService()
+    private val userService = FakeUserService()
+    private val context: Context = ApplicationProvider.getApplicationContext()
+
+    @Before
+    fun setup() {
+        vm = MainViewSelectorViewModel(
+            channelService,
+            messageService,
+            userService,
+            context
+        )
+    }
+
+    @Test
+    fun default_state_is_subscribed_channels() {
+        assert(vm.state is MainViewSelectorState.SubscribedChannels)
+    }
+
+    @Test
+    fun can_transition_state() {
+        assert(vm.state is MainViewSelectorState.SubscribedChannels)
+        vm.transition(MainViewSelectorState.ChannelMessages(authenticatedUser = authenticatedUser))
+        assert(vm.state is MainViewSelectorState.ChannelMessages)
+    }
+
+    @Test
+    fun load_channel_messages_transitions_to_loading_state() {
+        assert(vm.state is MainViewSelectorState.SubscribedChannels)
+        runBlocking {
+            vm.loadSubscribedChannels()
+            assert(vm.state is MainViewSelectorState.Loading)
+        }
+    }
+}
--- app/src/main/java/com/leic52dg17/chimp/ui/viewmodels/screen/MainViewSelectorViewModel.kt
+++ app/src/main/java/com/leic52dg17/chimp/ui/viewmodels/screen/MainViewSelectorViewModel.kt
@@ -11,13 +11,13 @@ import androidx.lifecycle.viewModelScope
 import com.leic52dg17.chimp.core.shared.SharedPreferencesHelper
 import com.leic52dg17.chimp.http.services.channel.IChannelService
 import com.leic52dg17.chimp.http.services.message.IMessageService
-import com.leic52dg17.chimp.domain.model.channel.Channel
 import com.leic52dg17.chimp.http.services.user.IUserService
-import com.leic52dg17.chimp.domain.model.auth.AuthenticatedUser
-import com.leic52dg17.chimp.domain.common.ErrorMessages
-import com.leic52dg17.chimp.domain.model.common.Failure
-import com.leic52dg17.chimp.domain.model.common.PermissionLevel
-import com.leic52dg17.chimp.domain.model.common.Success
+import com.leic52dg17.chimp.model.auth.AuthenticatedUser
+import com.leic52dg17.chimp.model.channel.Channel
+import com.leic52dg17.chimp.model.common.ErrorMessages
+import com.leic52dg17.chimp.model.common.Failure
+import com.leic52dg17.chimp.model.common.PermissionLevel
+import com.leic52dg17.chimp.model.common.Success
 import com.leic52dg17.chimp.ui.screens.main.MainViewSelectorState
 import kotlinx.coroutines.launch
 
