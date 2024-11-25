8a2058ae4048ff06aaef7afd308b31834316759c
Rúben Said <113713033+saidruben@users.noreply.github.com>
2024-11-25 16:35:03 +0000

    Features/incomming invitations view (#99)

--- app/src/main/java/com/leic52dg17/chimp/http/services/fake/FakeData.kt
+++ app/src/main/java/com/leic52dg17/chimp/http/services/fake/FakeData.kt
@@ -3,9 +3,11 @@ package com.leic52dg17.chimp.http.services.fake
 import com.leic52dg17.chimp.domain.model.channel.Channel
 import com.leic52dg17.chimp.domain.model.channel.ChannelInvitation
 import com.leic52dg17.chimp.domain.model.channel.UserChannel
+import com.leic52dg17.chimp.domain.model.common.PermissionLevel
 import com.leic52dg17.chimp.domain.model.message.Message
 import com.leic52dg17.chimp.domain.model.user.User
 import java.math.BigInteger
+import java.util.UUID
 
 object FakeData {
     val users = mutableListOf(
@@ -30,8 +32,6 @@ object FakeData {
             "User 4"
         )
     )
-
-    val channelInvitations = mutableListOf<ChannelInvitation>()
 
     val channels = mutableListOf(
         Channel(
@@ -61,6 +61,88 @@ object FakeData {
             channelIconUrl = "https://fake.com/not-real",
             isPrivate = false,
             ownerId = 1
+        ),
+        Channel(
+            channelId = 2,
+            displayName = "Channel 2",
+            messages = listOf(
+                Message(
+                    3,
+                    2,
+                    channelId = 2,
+                    "Welcome to Channel 2",
+                    BigInteger("21031239131231400")
+                )
+            ),
+            users = listOf(
+                users[1],
+                users[2],
+                users[3]
+            ),
+            channelIconUrl = "https://fake.com/not-real",
+            isPrivate = true,
+            ownerId = 2
+        ),
+        Channel(
+            channelId = 3,
+            displayName = "Channel 3",
+            messages = listOf(
+                Message(
+                    4,
+                    3,
+                    channelId = 3,
+                    "Channel 3 is live",
+                    BigInteger("21031239131231500")
+                )
+            ),
+            users = listOf(
+                users[0],
+                users[3]
+            ),
+            channelIconUrl = "https://fake.com/not-real",
+            isPrivate = false,
+            ownerId = 3
+        ),
+        Channel(
+            channelId = 4,
+            displayName = "Channel 4",
+            messages = listOf(
+                Message(
+                    5,
+                    4,
+                    channelId = 4,
+                    "Channel 4 announcement",
+
+                    BigInteger("21031239131231600")
+                )
+            ),
+            users = listOf(
+                users[2],
+                users[3]
+            ),
+            channelIconUrl = "https://fake.com/not-real",
+            isPrivate = true,
+            ownerId = 4
+        ),
+        Channel(
+            channelId = 5,
+            displayName = "Channel 5",
+            messages = listOf(
+                Message(
+                    6,
+                    5,
+                    channelId = 5,
+                    "Welcome to Channel 5",
+                    BigInteger("21031239131231700")
+                )
+            ),
+            users = listOf(
+                users[0],
+                users[1]
+            ),
+            channelIconUrl = "https://fake.com/not-real",
+            isPrivate = false,
+            ownerId = 1
         )
     )
 
@@ -79,6 +161,45 @@ object FakeData {
             3,
             1,
             BigInteger("1603999100")
+        )
+    )
+    val channelInvitations = mutableListOf<ChannelInvitation>()
+
+    val invitations = mutableListOf(
+        ChannelInvitation(
+            invitationId = UUID.randomUUID(),
+            channelId = 1,
+            senderId = 2,
+            receiverId = 1,
+            permissionLevel = PermissionLevel.RW
+        ),
+        ChannelInvitation(
+            invitationId = UUID.randomUUID(),
+            channelId = 2,
+            senderId = 2,
+            receiverId = 1,
+            permissionLevel = PermissionLevel.RW
+        ),
+        ChannelInvitation(
+            invitationId = UUID.randomUUID(),
+            channelId = 3,
+            senderId = 3,
+            receiverId = 1,
+            permissionLevel = PermissionLevel.RW
+        ),
+        ChannelInvitation(
+            invitationId = UUID.randomUUID(),
+            channelId = 4,
+            senderId = 4,
+            receiverId = 1,
+            permissionLevel = PermissionLevel.RW
+        ),
+        ChannelInvitation(
+            invitationId = UUID.randomUUID(),
+            channelId = 5,
+            senderId = 1,
+            receiverId = 1,
+            permissionLevel = PermissionLevel.RW
         )
     )
 }
